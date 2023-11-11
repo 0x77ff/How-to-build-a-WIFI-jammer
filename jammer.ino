4a37cdf858b77de4eac76810740fa9693b26a8ee
0x77ff <116833814+0x77ff@users.noreply.github.com>
2023-11-11 20:55:02 +0000

    Update jammer.ino

--- jammer.ino
+++ jammer.ino
@@ -7,14 +7,14 @@ RF24 radio(9, 10); // CE & CSN
 void setup() {
   radio.begin();
   radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_2MPS);
+  radio.setDataRate(RF24_MPS);
   radio.setChannel(22);
   radio.stopListening();
 }
 void loop() {
   for(int i=16; i<38; i++){
     radio.setChannel(i);
-    const char text[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const char text[] = "XXXXXXXXXXXXXX";
     radio.write(&text, sizeof(text));
   }
 }
